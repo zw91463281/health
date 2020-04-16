6902d1260dfc910471e26a308b31a4fd36f7be00
墨肃 <914632881@qq.com>
2020-04-16 23:00:53 +0800

    demo类删掉

---
+++ src/main/java/com/tyust/health/dto/demo.java
@@ -0,0 +1,9 @@
+package com.tyust.health.dto;
+
+/**
+ * @Author 郭志武
+ * @Date 2020/4/16 22:59
+ * @Version 1.0
+ */
+public class demo {
+}
---
+++ src/main/java/com/tyust/health/util/demo.java
@@ -0,0 +1,9 @@
+package com.tyust.health.util;
+
+/**
+ * @Author 郭志武
+ * @Date 2020/4/16 22:59
+ * @Version 1.0
+ */
+public class demo {
+}
